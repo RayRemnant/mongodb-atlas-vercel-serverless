a7934146d2c58af06cae3ac26066ed015d3699f3
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-04-24 21:02:01 +0200

    implementing deleteMany

---
+++ api/deleteMany.js
@@ -0,0 +1,11 @@
+import handler from "../lib/handler";
+
+export default async (req, res) => {
+	const collection = await handler(req);
+
+	const { acknowledged, insertedId } = await collection.deleteMany(
+		req.body.query
+	);
+
+	res.status(200).json({ acknowledged, insertedId });
+};
