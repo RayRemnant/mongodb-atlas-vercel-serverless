6390da56eedd375a62c41c7e676caa42ff0b8779
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-04-24 10:27:40 +0200

    works, i guess.

--- api/replaceMany.js
+++ api/replaceMany.js
@@ -2,18 +2,23 @@ import handler from "../lib/handler";
 import replaceOne from "./replaceOne";
 
 export default async (req, res) => {
+	const collection = await handler(req);
+
 	const docs = req.body.data;
 
-	docs.forEach((doc) => {
-		replaceOne({
-			body: {
-				data: doc,
-				filter: { _id: doc._id },
-				databaseName: req.body.databaseName,
-				collectionName: req.body.collectionName,
+	docs.forEach((doc, index, array) => {
+		replaceOne(
+			{
+				body: {
+					collection,
+					data: doc,
+					filter: { _id: doc._id },
+					databaseName: req.body.databaseName,
+					collectionName: req.body.collectionName,
+				},
 			},
-		});
+			//if last iteration, pass the res to respond to the client
+			index === array.length - 1 ? res : undefined
+		);
 	});
-
-	res.status(200).json(responseData);
 };
--- api/replaceOne.js
+++ api/replaceOne.js
@@ -1,17 +1,18 @@
 import handler from "../lib/handler";
-import { ObjectId } from 'mongodb'
+import { ObjectId } from "mongodb";
 
 export default async (req, res) => {
-	const collection = await handler(req)
+	//if called from replaceMany, collection is passed
+	const collection = req.body.collection || (await handler(req));
 
-	const { filter, data: doc } = req.body
+	console.log("COLL ", collection);
+	const { filter, data: doc } = req.body;
 
-	const responseData = await collection.replaceOne(filter, {
-		_id: new ObjectId(),
-		...doc,
-	}, { upsert: true })
+	const responseData = await collection.replaceOne(filter, doc, {
+		upsert: true,
+	});
 
-	console.log(`replaceOne OK ${JSON.stringify(responseData)}`)
+	console.log(`replaceOne OK`);
 
-	res ? res.status(200).json(responseData) : "replaceMany"
-}
+	res ? res.status(200).json(responseData) : "";
+};
