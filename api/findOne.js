3a2a3d81befd6ce97c7cade5a8097e5d8f75d6d3
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-04-24 10:27:24 +0200

    find -> findOne

--- api/findOne.js
+++ api/findOne.js
@@ -1,9 +1,9 @@
 import handler from "../lib/handler";
 
 export default async (req, res) => {
-	const collection = await handler(req)
+	const collection = await handler(req);
 
-	const results = await collection.find({})
+	const results = await collection.findOne({});
 
 	res.status(200).json(results);
-}
+};
