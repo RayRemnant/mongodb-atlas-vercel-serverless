bc09127401924f93f18b39114c9ba96deeea941f
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-04-29 13:46:33 +0200

    adding filter parameter

--- api/findOne.js
+++ api/findOne.js
@@ -2,8 +2,9 @@ import handler from "../lib/handler";
 
 export default async (req, res) => {
 	const collection = await handler(req);
+	const { filter } = req.body;
 
-	const results = await collection.findOne({});
+	const results = await collection.findOne(filter);
 
 	res.status(200).json(results);
 };
