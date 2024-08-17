1a6679337d04e9437a2a09e1b07b262e1681429c
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-08-17 13:38:04 +0200

    adding filter option

--- api/find.js
+++ api/find.js
@@ -2,8 +2,9 @@ import handler from "../lib/handler";
 
 export default async (req, res) => {
 	const collection = await handler(req);
+	const { filter } = req.body;
 
-	const results = await collection.find({}).toArray();
+	const results = await collection.find(filter).toArray();
 
 	res.status(200).json(results);
 };
