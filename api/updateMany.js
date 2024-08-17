577275f0871edde1cdddfb5e3ead6f53b08b9bfe
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-08-17 14:48:25 +0200

    added possibilit of more complex updates

--- api/updateMany.js
+++ api/updateMany.js
@@ -1,15 +1,25 @@
 import handler from "../lib/handler";
 
 export default async (req, res) => {
-	const collection = await handler(req)
+	const collection = await handler(req);
 
-	const { filter, data: update } = req.body;
+	const {
+		filter,
+		data,
+		update = data
+			? {
+					$set: {
+						...data,
+					},
+			  }
+			: update,
+	} = req.body;
 
-	const { acknowledged, modifiedCount } = await collection.updateMany(filter, {
-		$set: {
-			...update
-		}
-	}, { upsert: true })
+	const { acknowledged, modifiedCount } = await collection.updateMany(
+		filter,
+		update,
+		{ upsert: true }
+	);
 
-	res.status(200).json({ acknowledged, insertedId })
-}
+	res.status(200).json({ acknowledged, modifiedCount });
+};
