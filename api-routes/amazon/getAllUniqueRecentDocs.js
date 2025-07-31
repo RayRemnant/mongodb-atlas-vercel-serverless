866ebc14d21927cfc1b6baac5245e6578b870b37
rayremnant <rayremnant.me@gmail.com>
2025-07-31 19:45:25 +0200

    feat: add filter parameter

--- api-routes/amazon/getAllUniqueRecentDocs.js
+++ api-routes/amazon/getAllUniqueRecentDocs.js
@@ -1,21 +1,24 @@
 import handler from "../../lib/handler.js";
 
 export default async (req, res) => {
+		const { filter } = req.body;
+
     const collection = await handler({
         body: { databaseName: "codex", collectionName: "amazon" },
     });
 
-    // Aggregation pipeline to group by "asin" and "region" and select the document with the most recent "lastUpdated"
-    const aggregationPipeline = [
-        { $sort: { asin: 1, region: 1, lastUpdated: -1 } }, // sort by asin, region, and most recent lastUpdated
-        {
-            $group: {
-                _id: { asin: "$asin", region: "$region" },
-                doc: { $first: "$$ROOT" }
-            }
-        },
-        { $replaceRoot: { newRoot: "$doc" } }
-    ];
+    // Aggregation pipeline to group by "asin" and "region" and select the document with the most recent "createdAt"
+	const aggregationPipeline = [
+		{ $match: filter || {} }, // Apply the filter object if provided
+		{ $sort: { asin: 1, region: 1, createdAt: -1 } }, // sort by asin, region, and most recent createdAt
+		{
+			$group: {
+				_id: { asin: "$asin", region: "$region" },
+				doc: { $first: "$$ROOT" }
+			}
+		},
+		{ $replaceRoot: { newRoot: "$doc" } }
+	];
 
     const uniqueDocuments = await collection
         .aggregate(aggregationPipeline)
